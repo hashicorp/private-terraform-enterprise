73c48f9f1e52ee02efceb40b191c37f74e72bec6
ausfestivus <abest@diaxion.com>
2019-11-08 10:50:03 +1100

    moved kv access policies to kv resource

--- examples/bootstrap-azure/key_vault.tf
+++ examples/bootstrap-azure/key_vault.tf
@@ -10,14 +10,11 @@ resource "azurerm_key_vault" "new" {
   tags                            = "${local.tags}"
   enabled_for_deployment          = true
   enabled_for_template_deployment = true
-}
 
-# access policy for the ecurrent signed in user building the vault.
-resource "azurerm_key_vault_access_policy" "tf-user" {
-  key_vault_id = "${azurerm_key_vault.new.id}"
-  tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-  object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
-  key_permissions = [
+  access_policy {                             # access policy for the current signed in user building the vault.
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
+    key_permissions = [
     "backup",
     "create",
     "decrypt",
@@ -34,9 +31,8 @@ resource "azurerm_key_vault_access_policy" "tf-user" {
     "update",
     "verify",
     "wrapKey",
-  ]
-
-  secret_permissions = [
+    ]
+    secret_permissions = [
     "backup",
     "delete",
     "get",
@@ -45,7 +41,7 @@ resource "azurerm_key_vault_access_policy" "tf-user" {
     "recover",
     "restore",
     "set",
-  ]
+    ]
     certificate_permissions = [
     "create",
     "delete",
@@ -59,65 +55,171 @@ resource "azurerm_key_vault_access_policy" "tf-user" {
     "manageissuers",
     "setissuers",
     "update",
-  ]
+    ]
+  }
+  
+  access_policy {                             # access policy for the required/created/dedicated/selected keyvault SP user
+    tenant_id = "${var.key_vault_tenant_id}"
+    object_id = "${var.key_vault_object_id}"
+    key_permissions = [
+      "get",
+      "list",
+      "update",
+      "create",
+      "import",
+      "delete",
+    ]
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete",
+    ]
+    certificate_permissions = [
+      "get",
+      "list",
+      "update",
+      "create",
+      "import",
+      "delete",
+    ]  
+  }
+
+  access_policy {                             # access policy for the required/created/dedicated/selected keyvault SP user
+    tenant_id = "${var.key_vault_tenant_id}"
+    object_id = "${var.key_vault_object_id}"
+    application_id = "${var.application_id}"
+    key_permissions = [
+      "get",
+      "list",
+      "update",
+      "create",
+      "import",
+      "delete",
+    ]
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete",
+    ]
+    certificate_permissions = [
+      "get",
+      "list",
+      "update",
+      "create",
+      "import",
+      "delete",
+    ]
+  }
 }
 
-# access policy for the required/created/dedicated/selected keyvault SP user
-resource "azurerm_key_vault_access_policy" "new-user" {
-  key_vault_id = "${azurerm_key_vault.new.id}"
-  tenant_id = "${var.key_vault_tenant_id}"
-  object_id = "${var.key_vault_object_id}"
-  key_permissions = [
-    "get",
-    "list",
-    "update",
-    "create",
-    "import",
-    "delete",
-  ]
-  secret_permissions = [
-    "get",
-    "list",
-    "set",
-    "delete",
-  ]
-  certificate_permissions = [
-    "get",
-    "list",
-    "update",
-    "create",
-    "import",
-    "delete",
-  ]
-}
+# # access policy for the current signed in user building the vault.
+# resource "azurerm_key_vault_access_policy" "tf-user" {
+#   key_vault_id = "${azurerm_key_vault.new.id}"
+#   tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+#   object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
+#   key_permissions = [
+#     "backup",
+#     "create",
+#     "decrypt",
+#     "delete",
+#     "encrypt",
+#     "get",
+#     "import",
+#     "list",
+#     "purge",
+#     "recover",
+#     "restore",
+#     "sign",
+#     "unwrapKey",
+#     "update",
+#     "verify",
+#     "wrapKey",
+#   ]
 
-# access policy for the required/created/dedicated/selected keyvault SP user
-resource "azurerm_key_vault_access_policy" "new-app" {
-  key_vault_id = "${azurerm_key_vault.new.id}"
-  tenant_id = "${var.key_vault_tenant_id}"
-  object_id = "${var.key_vault_object_id}"
-  application_id = "${var.application_id}"
-  key_permissions = [
-    "get",
-    "list",
-    "update",
-    "create",
-    "import",
-    "delete",
-  ]
-  secret_permissions = [
-    "get",
-    "list",
-    "set",
-    "delete",
-  ]
-  certificate_permissions = [
-    "get",
-    "list",
-    "update",
-    "create",
-    "import",
-    "delete",
-  ]
-}
+#   secret_permissions = [
+#     "backup",
+#     "delete",
+#     "get",
+#     "list",
+#     "purge",
+#     "recover",
+#     "restore",
+#     "set",
+#   ]
+#     certificate_permissions = [
+#     "create",
+#     "delete",
+#     "deleteissuers",
+#     "get",
+#     "getissuers",
+#     "import",
+#     "list",
+#     "listissuers",
+#     "managecontacts",
+#     "manageissuers",
+#     "setissuers",
+#     "update",
+#   ]
+# }
+
+# # access policy for the required/created/dedicated/selected keyvault SP user
+# resource "azurerm_key_vault_access_policy" "new-user" {
+#   key_vault_id = "${azurerm_key_vault.new.id}"
+#   tenant_id = "${var.key_vault_tenant_id}"
+#   object_id = "${var.key_vault_object_id}"
+#   key_permissions = [
+#     "get",
+#     "list",
+#     "update",
+#     "create",
+#     "import",
+#     "delete",
+#   ]
+#   secret_permissions = [
+#     "get",
+#     "list",
+#     "set",
+#     "delete",
+#   ]
+#   certificate_permissions = [
+#     "get",
+#     "list",
+#     "update",
+#     "create",
+#     "import",
+#     "delete",
+#   ]
+# }
+
+# # access policy for the required/created/dedicated/selected keyvault SP user
+# resource "azurerm_key_vault_access_policy" "new-app" {
+#   key_vault_id = "${azurerm_key_vault.new.id}"
+#   tenant_id = "${var.key_vault_tenant_id}"
+#   object_id = "${var.key_vault_object_id}"
+#   application_id = "${var.application_id}"
+#   key_permissions = [
+#     "get",
+#     "list",
+#     "update",
+#     "create",
+#     "import",
+#     "delete",
+#   ]
+#   secret_permissions = [
+#     "get",
+#     "list",
+#     "set",
+#     "delete",
+#   ]
+#   certificate_permissions = [
+#     "get",
+#     "list",
+#     "update",
+#     "create",
+#     "import",
+#     "delete",
+#   ]
+# }
 
