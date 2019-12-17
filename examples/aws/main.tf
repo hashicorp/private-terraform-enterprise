b45c8b81c04be32e579d328b5980ebbe357599c0
ml4 <ml4@psy.industries>
2019-12-17 16:49:33 +0000

    Fix main.tf to point to running one of the three types of deploy at a time

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -57,37 +57,37 @@ module "demo" {
 #------------------------------------------------------------------------------
 # production mounted disk ptfe 
 #------------------------------------------------------------------------------
-
-module "pmd" {
-  source                 = "pmd/"
-  namespace              = "${var.namespace}"
-  aws_instance_ami       = "${var.aws_instance_ami}"
-  aws_instance_type      = "${var.aws_instance_type}"
-  subnet_id              = "${module.network.subnet_ids[0]}"
-  vpc_security_group_ids = "${module.network.security_group_id}"
-  user_data              = "${data.template_file.user_data.rendered}"
-  ssh_key_name           = "${var.ssh_key_name}"
-  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
-  owner                  = "${var.owner}"
-  ttl                    = "${var.ttl}"
-}
-
-#------------------------------------------------------------------------------
-# production external-services ptfe 
-#------------------------------------------------------------------------------
-
-module "pes" {
-  source                 = "pes/"
-  namespace              = "${var.namespace}"
-  aws_instance_ami       = "${var.aws_instance_ami}"
-  aws_instance_type      = "${var.aws_instance_type}"
-  subnet_ids             = "${module.network.subnet_ids}"
-  vpc_security_group_ids = "${module.network.security_group_id}"
-  user_data              = "${data.template_file.user_data.rendered}"
-  ssh_key_name           = "${var.ssh_key_name}"
-  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
-  database_pwd           = "${random_pet.replicated-pwd.id}"
-  db_subnet_group_name   = "${module.network.db_subnet_group_id}"
-  owner                  = "${var.owner}"
-  ttl                    = "${var.ttl}"
-}
+#
+#module "pmd" {
+#  source                 = "pmd/"
+#  namespace              = "${var.namespace}"
+#  aws_instance_ami       = "${var.aws_instance_ami}"
+#  aws_instance_type      = "${var.aws_instance_type}"
+#  subnet_id              = "${module.network.subnet_ids[0]}"
+#  vpc_security_group_ids = "${module.network.security_group_id}"
+#  user_data              = "${data.template_file.user_data.rendered}"
+#  ssh_key_name           = "${var.ssh_key_name}"
+#  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
+#  owner                  = "${var.owner}"
+#  ttl                    = "${var.ttl}"
+#}
+#
+##------------------------------------------------------------------------------
+## production external-services ptfe 
+##------------------------------------------------------------------------------
+#
+#module "pes" {
+#  source                 = "pes/"
+#  namespace              = "${var.namespace}"
+#  aws_instance_ami       = "${var.aws_instance_ami}"
+#  aws_instance_type      = "${var.aws_instance_type}"
+#  subnet_ids             = "${module.network.subnet_ids}"
+#  vpc_security_group_ids = "${module.network.security_group_id}"
+#  user_data              = "${data.template_file.user_data.rendered}"
+#  ssh_key_name           = "${var.ssh_key_name}"
+#  hashidemos_zone_id     = "${data.aws_route53_zone.hashidemos.zone_id}"
+#  database_pwd           = "${random_pet.replicated-pwd.id}"
+#  db_subnet_group_name   = "${module.network.db_subnet_group_id}"
+#  owner                  = "${var.owner}"
+#  ttl                    = "${var.ttl}"
+#}
